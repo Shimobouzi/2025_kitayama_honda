2f3e2579e4183f8aa5480c58cde1229cd19842b5
megane <e1b23021@oit.ac.jp>
2025-10-02 16:28:41 +0900

    ピッチャーと球を飛ばす関数

---
+++ KitayamaBatting/Assets/Main/Script/pitcher.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class pitcher : MonoBehaviour
+{
+     [SerializeField] GameObject sphere;
+    private float speed = 1300;
+ 
+    void Update ()
+    {
+        
+    }
+   public void ball(InputAction.CallbackContext context){
+    if(!context.performed) return;
+        GameObject ball = (GameObject)Instantiate(sphere, transform.position, Quaternion.identity);
+            Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+            ballRigidbody.AddForce(transform.forward * speed);
+    }
+
+}
